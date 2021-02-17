186b65d0affe3ebc52c1e271603971b385e514df
Mike North <michael.l.north@gmail.com>
2021-02-16 22:06:30 -0800

    Initial commit

---
+++ src/calc.py
@@ -0,0 +1,31 @@
+
+def add(a, b):
+    """
+    Add some numbers together
+
+    ```py
+    add(2, 3) # 5
+    ```
+    """
+    return a + b
+
+def sub(a, b):
+    """
+    Subtract some numbers
+
+    ```py
+    sub(8, 3) # 5
+    ```
+    """
+    return a - b
+
+
+def mult(a, b):
+    """
+    Multiply some numbers
+
+    ```py
+    mult(8, 3) # 24
+    ```
+    """
+    return a * b
